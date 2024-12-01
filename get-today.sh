1ec4fa1bb2e37c63fd5f195f54be60d4b0820dc9
Peter LeBrun <peterlebrun@gmail.com>
2024-12-01 15:52:08 -0500

    small fix

--- get-today.sh
+++ get-today.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 source ./get-cookie.sh
-TODAY=$(date +'%d')
+TODAY_LEADING_ZERO=$(date +'%d')
+TODAY=$(date +'%-d')
 
+echo $TODAY_LEADING_ZERO
 echo $TODAY
 
 for i in {01,02};
 do
-    mkdir -p $TODAY/$i
+    mkdir -p $TODAY_LEADING_ZERO/$i
 
     curl \
     --cookie $COOKIE \
-    https://adventofcode.com/2023/day/$TODAY/input \
-    -o $TODAY/$i/input
+    https://adventofcode.com/2024/day/$TODAY/input \
+    -o $TODAY_LEADING_ZERO/$i/input
 
-    touch $TODAY/$i/script.py
-    echo "with open('input', 'r') as f:" > $TODAY/$i/script.py
+    touch $TODAY_LEADING_ZERO/$i/script.py
+    echo "with open('input', 'r') as f:" > $TODAY_LEADING_ZERO/$i/script.py
 done
