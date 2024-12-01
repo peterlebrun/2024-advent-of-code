459cee369ee226cc95b5072154a20672d4c53536
Peter LeBrun <peterlebrun@gmail.com>
2024-12-01 16:16:09 -0500

    day 1 complete

--- 01/01/script.py
+++ 01/01/script.py
@@ -1,1 +1,16 @@
+list1 = []
+list2 = []
+diff = 0
+
 with open('input', 'r') as f:
+    for line in f:
+        nums = [int(x) for x in line.split()]
+        list1.append(nums[0])
+        list2.append(nums[1])
+
+list1.sort()
+list2.sort()
+
+for n in range(len(list1)):
+    diff = diff + abs(list1[n] - list2[n])
+print(diff)
--- 01/02/script.py
+++ 01/02/script.py
@@ -1,1 +1,17 @@
+from collections import defaultdict
+list1 = defaultdict(int)
+list2 = defaultdict(int)
+similarity_score = 0
+
 with open('input', 'r') as f:
+    for line in f:
+        nums = [x for x in line.split()]
+        list1[nums[0]] = list1[nums[0]] + 1
+        list2[nums[1]] = list2[nums[1]] + 1
+
+for num in list1.keys():
+    if num in list2:
+        print(int(num), list1[num], list2[num])
+        similarity_score = similarity_score + (int(num) * list1[num] * list2[num])
+
+print(similarity_score)
