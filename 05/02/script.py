89ffb2980def3c29c6f9a58888ea6d7f392303d4
Peter LeBrun <peterlebrun@gmail.com>
2024-12-06 01:23:28 -0500

    part 2 of day 5

--- 05/02/script.py
+++ 05/02/script.py
@@ -1,82 +1,105 @@
 from collections import defaultdict
-import sys
-sys.setrecursionlimit(1073741824)
 
 PIPE = "|"
 COMMA = ","
 
-rules = {}
-rules_tree = {}
-#priority = defaultdict(int)
-unordered_updates = []
-middle_sum = 0
+rules = defaultdict(set)
+updates = []
+sorted_middle_sum = 0
+unsorted_middle_sum = 0
 
 def is_in_order(input_str, rules):
+    in_order = True
+    applicable_rules = []
     vals = input_str.split(COMMA)
 
     for v in vals:
-        #print(f"v: {v}")
         v_start = input_str.find(v)
-        #print(f"v_start: {v_start}")
 
         try:
             for z in rules[v]:
-                #print(f"z: {z}")
                 z_start = input_str.find(z)
-                #print(f"z_start: {z_start}")
 
                 if z_start == -1:
                     continue
 
                 if z_start < v_start:
-                    #print("Not in order")
-                    return False
+                    in_order = False
+
+                applicable_rules.append(f"{v}|{z}")
         except KeyError:
             continue
 
-    #print("In order")
-    return True
+    return in_order, applicable_rules
 
-priorities = []
+def create_mapping():
+    mapping = {}
+    for x in rules:
+        for y in rules:
+            intersection = rules[x].intersection(rules[y])
+            if len(intersection) == 23:
+                x_diff = rules[x] - intersection
+                if x_diff == set([y]):
+                    mapping[x] = y
+    return mapping
 
-used_nums = set()
+def create_sort_list(start):
+    counter = 1
+    priorities = { start: counter }
+
+    val = mapping[start]
+    while val != start:
+        counter += 1
+        priorities[val] = counter
+        val = mapping[val]
+
+    return priorities
+
+def get_sorted_list(input_str, start):
+    arr = input_str.split(COMMA)
+    sort_list = create_sort_list(start)
+    return ','.join(sorted(arr, key=lambda x: sort_list[x]))
+
+def get_middle_val(input_str):
+    arr = input_str.split(COMMA)
+    middle = int((len(arr) - 1) / 2)
+    return int(arr[middle])
+
 with open('input', 'r') as f:
     for x in [r.strip() for r in f.readlines()]:
         if PIPE in x:
             pre, post = x.split(PIPE)
-
-#case: pre in priorities (but not post)
-#case: post in priorites (but not pre)
-#case: neither in priorities
-#case: both in priorities
-
-
-            rules[pre].append(post)
-
-            pre_vals[pre] += 1
-            post_vals[post] += 1
-            print("pre_vals")
-            print(pre_vals)
-            print("post_vals")
-            print(post_vals)
-            #input()
+            rules[pre].add(post)
 
         elif COMMA in x:
-            if not is_in_order(x, rules):
-                unordered_updates.append(x)
-                for y in x.split(COMMA):
-                    used_nums.add(y)
-                #un
-                #int_vals = list(map(int, x.split(COMMA)))
-                #print(f"int_vals: {int_vals}")
-                #middle_index = int((len(int_vals) - 1) / 2)
-                #print(f"middle_index: {middle_index}")
-                #middle_sum += int_vals[middle_index]
-                #print(f"middle_sum: {middle_sum}")
+            updates.append(x)
 
-#print(middle_sum)
-def get_child_rules(pre):
-    for r in rules[pre]:
-        return get_child_rules(r)
+mapping = create_mapping()
 
-START = list(rules.keys())[0]
+for u in updates:
+    in_order, applicable_rules = is_in_order(u, rules)
+
+    if in_order:
+        sorted_middle_sum += int(get_middle_val(u))
+        continue
+
+    else:
+        pres = set()
+        posts = set()
+
+        for r in applicable_rules:
+            pre, post = r.split("|")
+            pres.add(pre)
+            posts.add(post)
+
+        start = None
+        for x in pres:
+            if x not in posts:
+                start = x
+                break
+
+        sorted_list = get_sorted_list(u, start)
+        unsorted_middle_sum += int(get_middle_val(sorted_list))
+
+print(sorted_middle_sum)
+print(unsorted_middle_sum)
