734214ab2191aba596a4dc111ad710dd70dbb415
Peter LeBrun <peterlebrun@gmail.com>
2024-12-07 01:39:30 -0500

    day 7 boilerplate

---
+++ 07/02/script.py
@@ -0,0 +1,9 @@
+import sys
+import copy
+from collections import defaultdict
+sys.setrecursionlimit(1073741824)
+
+INPUT = "input_test" if sys.argv[1] == "--test" else "input"
+
+with open('input', 'r') as f:
+
