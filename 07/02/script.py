409c3d55fe59118fd1965cbcde5458d0f170bf29
Peter LeBrun <peterlebrun@gmail.com>
2024-12-07 01:41:45 -0500

    update day 7

--- 07/02/script.py
+++ 07/02/script.py
@@ -3,7 +3,7 @@ import copy
 from collections import defaultdict
 sys.setrecursionlimit(1073741824)
 
-INPUT = "input_test" if sys.argv[1] == "--test" else "input"
+INPUT = "input_test" if len(sys.argv) > 1 and sys.argv[1] == "--test" else "input"
 
 with open('input', 'r') as f:
 
