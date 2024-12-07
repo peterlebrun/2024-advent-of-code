407d39cdcb5c267d7c4b98d0f85cee49dd42a6fa
Peter LeBrun <peterlebrun@gmail.com>
2024-12-07 01:39:09 -0500

    whoopsie

--- get-today.sh
+++ get-today.sh
@@ -23,7 +23,7 @@ import copy
 from collections import defaultdict
 sys.setrecursionlimit(1073741824)
 
-INPUT = "input_test" if sys.argv[2] == "--test" else "input"
+INPUT = "input_test" if sys.argv[1] == "--test" else "input"
 
 with open('input', 'r') as f:
 
