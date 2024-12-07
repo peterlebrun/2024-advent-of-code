9b631944fd9694c43ca5f33069cdba59e7c76e57
Peter LeBrun <peterlebrun@gmail.com>
2024-12-07 01:37:13 -0500

    Update get-today script to include more imports

--- get-today.sh
+++ get-today.sh
@@ -16,5 +16,16 @@ do
     -o $TODAY_LEADING_ZERO/$i/input
 
     touch $TODAY_LEADING_ZERO/$i/script.py
-    echo "with open('input', 'r') as f:" > $TODAY_LEADING_ZERO/$i/script.py
+
+    cat <<EOF > $TODAY_LEADING_ZERO/$i/script.py
+import sys
+import copy
+from collections import defaultdict
+sys.setrecursionlimit(1073741824)
+
+INPUT = "input_test" if sys.argv[2] == "--test" else "input"
+
+with open('input', 'r') as f:
+
+EOF
 done
