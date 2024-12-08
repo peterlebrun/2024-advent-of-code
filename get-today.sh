acd2d97e5041ad19eab9292b9a6c2929e76cda0d
Peter LeBrun <peterlebrun@gmail.com>
2024-12-08 16:46:39 -0500

    Update get-today script with better boilerplate

--- get-today.sh
+++ get-today.sh
@@ -6,30 +6,64 @@ TODAY=$(date +'%-d')
 echo $TODAY_LEADING_ZERO
 echo $TODAY
 
-for i in {01,02};
-do
-    mkdir -p $TODAY_LEADING_ZERO/$i
+mkdir -p $TODAY_LEADING_ZERO/01
 
-    curl \
-    --cookie $COOKIE \
-    https://adventofcode.com/2024/day/$TODAY/input \
-    -o $TODAY_LEADING_ZERO/$i/input
+curl \
+--cookie $COOKIE \
+https://adventofcode.com/2024/day/$TODAY/input \
+-o $TODAY_LEADING_ZERO/01/input
 
-    touch $TODAY_LEADING_ZERO/$i/script.py
-
-    cat <<EOF > $TODAY_LEADING_ZERO/$i/script.py
+cat <<EOF > $TODAY_LEADING_ZERO/01/script.py
 import sys
 import copy
 from collections import defaultdict
+import math
 sys.setrecursionlimit(1073741824)
 
-INPUT = "input_test" if len(sys.argv) > 1 and sys.argv[1] == "--test" else "input"
+def blue(text):
+    return f"\033[94m{text}\033[0m"
 
-def print_divider():
-    print("=" * 80)
-    print("\n")
+def green(text):
+    return f"\033[92m{text}\033[0m"
 
-with open(INPUT, 'r') as f:
+def red(text):
+    return f"\033[91m{text}\033[0m"
 
+def print_str(*args):
+    print(" ".join(args))
+
+def print_divider(divider="=", length=80):
+    print(divider*length, "\n")
+
+def exit():
+    print_str("Exiting...")
+    sys.exit()
+
+if len(sys.argv) < 2:
+    print_str("Please specify input file.")
+    exit()
+if sys.argv[1] not in ["input", "input_test"]:
+    print_str("Invalid input file provided. Should be one of ", blue("input "),
+              "or ", blue("input_test"))
+    exit()
+if len(sys.argv) > 2:
+    print_str("Unrecognized arguments provided.")
+    exit()
+
+INPUT = sys.argv[1]
+
+DOT = "."
+STAR = "*"
+HASH = "#"
+
+with open(INPUT, "r") as f:
+    inputs = [[c for c in r.strip()] for r in f.readlines()]
+
+num_rows = len(inputs)
+num_cols = len(inputs[0])
+
+for row in range(num_rows):
+    for col in range(num_cols):
+        pass
+        char = inputs[row][col]
 EOF
-done
