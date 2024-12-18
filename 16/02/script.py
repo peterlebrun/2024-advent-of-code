c20bc11e945bfcf53ac4d898074c5ba9c988c349
Peter LeBrun <peterlebrun@gmail.com>
2024-12-17 23:56:42 -0500

    Reallly great day of coding today.

--- 16/02/script.py
+++ 16/02/script.py
@@ -76,7 +76,7 @@ DELTAS = {
     WEST: [0, -1],
 }
 
-def print_grid(grid, coords, neighbors, missing_vals=[]):
+def print_grid(grid, coords=(None, None), neighbors=set(), missing_vals=[]):
     top = " " * 5
     middle = " " * 5
     bottom = " " * 5
@@ -105,7 +105,7 @@ def print_grid(grid, coords, neighbors, missing_vals=[]):
             if (row_index, col_index) == coords:
                 output += f"{g(col)}"
             elif (row_index, col_index) in neighbors:
-                output += f"{b(col)}"
+                output += b("0")
             elif col == WALL:
                 output += "\033[37;2m#\033[0m"
             elif (row_index, col_index) in missing_vals:
@@ -128,53 +128,95 @@ def get_neighbors(grid, row, col, direction):
         neighbors.append(((next_row, next_col, ndirection), 1))
     return neighbors
 
+def find_shortest_path(grid, start, end, blacklist = []):
+    print(f"Blacklist: {blacklist}")
+    dist = {}
+    prev = {}
+    unvisited_nodes = [start]
+    visited_nodes = set()
+    dist[start] = 0
+    end_nodes = set()
+    while len(unvisited_nodes):
+        unvisited_nodes.sort(key=lambda u: dist[u])
+        node = unvisited_nodes.pop(0)
+        visited_nodes.add(node)
+
+        for neighbor, cost in get_neighbors(grid, *node):
+            if (neighbor[0], neighbor[1]) in blacklist:
+                continue
+            if neighbor in visited_nodes:
+                continue
+            if (neighbor[0], neighbor[1]) == end:
+                end_nodes.add(neighbor)
+            elif neighbor not in unvisited_nodes:
+                unvisited_nodes.append(neighbor)
+            if neighbor not in dist:
+                dist[neighbor] = float('inf')
+            if neighbor not in prev:
+                prev[neighbor] = None
+
+            tmp = dist[node] + cost
+
+            if tmp < dist[neighbor]:
+                dist[neighbor] = tmp
+                prev[neighbor] = node
+
+    if not len(end_nodes):
+        print("Could not reach end")
+        return float('inf'), []
+
+    # This seems like an awful lot of work
+    end_nodes = [node for node in end_nodes] # Bad pattern - overwriting set w/ list so we can sort it
+    end_nodes.sort(key=lambda u: dist[u])
+    e = end_nodes[0]
+    min_distance = dist[e]
+
+    current_node = e
+    shortest_path = [current_node]
+    while current_node != start:
+        current_node = prev[current_node]
+        shortest_path.append(current_node)
+
+    return min_distance, shortest_path[::-1]
+
 grid = []
-start_coords = None, None
-end_coords = None, None
-nodes = []
+start = None, None, EAST
+end = None, None
 with open(sys.argv[1], "r") as f:
     for row_index, row in enumerate(f.readlines()):
         row = row.strip()
         grid.append([])
         for col_index, col in enumerate(row):
             if col in [START, EMPTY, END]:
-                coords = (row_index, col_index)
-                nodes.append(coords)
                 if col == START:
-                    start_coords = coords
+                    start = row_index, col_index, EAST
                 elif col == END:
-                    end_coords = coords
+                    end = row_index, col_index
             grid[-1].append(col)
 
-dist = {}
-prev = {}
-unvisited_nodes = [(*start_coords, EAST)]
-visited_nodes = set()
-dist[(*start_coords, EAST)] = 0
-end_nodes = set()
-while len(unvisited_nodes):
-    unvisited_nodes.sort(key=lambda u: dist[u])
-    node = unvisited_nodes.pop(0)
-    visited_nodes.add(node)
+print_grid(grid)
 
-    for neighbor, cost in get_neighbors(grid, *node):
-        if neighbor in visited_nodes:
-            continue
-        if (neighbor[0], neighbor[1]) == end_coords:
-            end_nodes.add(neighbor)
-        elif neighbor not in unvisited_nodes:
-            unvisited_nodes.append(neighbor)
-        if neighbor not in dist:
-            dist[neighbor] = float('inf')
-        if neighbor not in prev:
-            prev[neighbor] = None
+# This works but it's a little buggy - it misses one of the tiles in input_test_1
+# HOWEVER - it worked for the actual problem case, so good enough
+shortest_paths = []
+min_distance, shortest_path = find_shortest_path(grid, start, end)
+print_divider(DOT, QUARTER)
+print(f"{id('Min Distance')} {min_distance}")
+shortest_paths.append(shortest_path)
+for idx, node in enumerate(shortest_path):
+    print_divider(DASH, HALF)
+    print(f"{idx} out of {len(shortest_path)}")
+    if node == start or (node[0], node[1]) == end:
+        continue
+    distance, path = find_shortest_path(grid, start, end, [(node[0], node[1])])
+    print(distance)
+    if distance == min_distance:
+        print(f"Appending to shortest paths")
+        shortest_paths.append(path)
 
-        tmp = dist[node] + cost
-
-        if tmp < dist[neighbor]:
-            dist[neighbor] = tmp
-            prev[neighbor] = node
-
-for e in end_nodes:
-    print(f"{e}: {dist[e]}")
-    print(f"prev: {prev[e]}")
+shortest_path_nodes = set()
+for path in shortest_paths:
+    for (row, col, direction) in path:
+        shortest_path_nodes.add((row, col))
+print(f"Nodes in shortest paths: {len(shortest_path_nodes)}")
+print_grid(grid, start, shortest_path_nodes)
