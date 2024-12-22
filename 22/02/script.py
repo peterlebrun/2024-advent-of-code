07666aa5a24ddbd63f8c2a435fe063401326fd99
Peter LeBrun <peterlebrun@gmail.com>
2024-12-22 11:57:42 -0500

    day 22 part 2

---
+++ 22/02/script.py
@@ -0,0 +1,136 @@
+import sys
+import copy
+from collections import defaultdict
+import math
+sys.setrecursionlimit(1073741824)
+
+ZERO = "0"
+ONE = "1"
+DOT = "."
+STAR = "*"
+HASH = "#"
+DASH = "-"
+EQUAL = "="
+FULL = 80
+HALF = 40
+QUARTER = 20
+TINY = 10
+
+def b(text):
+    return f"\033[94m{text}\033[0m"
+
+def g(text):
+    return f"\033[92m{text}\033[0m"
+
+def r(text):
+    return f"\033[91m{text}\033[0m"
+
+def lead_zero(num, num_digits = 2):
+    zeroes = num_digits - len(str(num))
+    return f"{'0' * zeroes}{num}" if zeroes >= 0 else f"{num}"
+
+def rpad(text, length = 5):
+    text = str(text)
+    spaces = length - len(text)
+    return f"{text}{' ' * spaces if spaces > 0 else ''}"
+
+def lpad(text, length = 5):
+    text = str(text)
+    spaces = length - len(text)
+    return f"{' ' * spaces if spaces > 0 else ''}{text}"
+
+def id(num, length = 12):
+    return b(rpad(f"{num}:", length))
+
+def print_divider(divider=EQUAL, length=FULL):
+    print(divider*length)
+
+def print_grid(grid, coords=(None, None), neighbors=set(), missing_vals=[]):
+    top = " " * 5
+    middle = " " * 5
+    bottom = " " * 5
+    for i in range(len(grid[0])):
+        if i < 10:
+            top += " "
+            middle += " "
+            bottom += f"{i}"
+            continue
+        elif i < 100:
+            i = str(i)
+            top += f" "
+            middle += f"{i[0]}"
+            bottom += f"{i[1]}"
+        else:
+            i = str(i)
+            top += f"{i[0]}"
+            middle += f"{i[1]}"
+            bottom += f"{i[2]}"
+    print(top)
+    print(middle)
+    print(bottom)
+    for row_index, row in enumerate(grid):
+        output = ""
+        for col_index, col in enumerate(row):
+            if (row_index, col_index) == coords:
+                output += f"{g(col)}"
+            elif (row_index, col_index) in neighbors:
+                output += b("0")
+            elif col == WALL:
+                output += "\033[37;2m#\033[0m"
+            elif (row_index, col_index) in missing_vals:
+                output += f"\033[91;1;4m{col}\033[0m"
+            else:
+                output += f"\033[32;1m{col}\033[0m"
+        print(f"{lpad(row_index, 3)}: {output}")
+
+def exit():
+    print("Exiting...")
+    sys.exit()
+
+if len(sys.argv) < 2:
+    print("Please specify input file.")
+    exit()
+
+if not sys.argv[1].startswith("input"):
+    print("Invalid input file provided.")
+    exit()
+
+if len(sys.argv) > 2:
+    print("Unrecognized arguments provided.")
+    exit()
+
+PRUNE_CONST = 0b111111111111111111111111
+
+def iterate(s0):
+        s1 = (s0^(s0<<6)) & PRUNE_CONST
+        s2 = (s1^(s1>>5)) & PRUNE_CONST
+        return (s2^(s2<<11)) & PRUNE_CONST
+
+def get_sequences(init, iterations=2000):
+    prices = [init % 10]
+    deltas = []
+    sequences = defaultdict(int)
+    prev = init
+    for _ in range(iterations):
+        prev = iterate(prev)
+        prices.append(prev % 10)
+        deltas.append(prices[-1] - prices[-2])
+
+        if len(deltas) >= 4:
+            s = ",".join(map(str, deltas[-4:]))
+            if s not in sequences:
+                sequences[s] = prices[-1]
+
+    return sequences
+
+result = 0
+counter = defaultdict(int)
+with open(sys.argv[1], "r") as f:
+    for i, row in enumerate(f.readlines()):
+        for s, val in get_sequences(int(row.strip())).items():
+            counter[s] += val
+
+print_divider(DASH, HALF)
+for k in sorted(counter.keys(), key=lambda x: counter[x], reverse=True):
+    print(f"{k}, {counter[k]}")
+    break
