854596e32a84f47f15910265b647f1eec04b65a7
Peter LeBrun <peterlebrun@gmail.com>
2024-12-23 10:01:32 -0500

    Day 23 part 2

---
+++ 23/02/script.py
@@ -0,0 +1,125 @@
+import sys
+import copy
+from collections import defaultdict
+import math
+sys.setrecursionlimit(1073741824)
+
+ZERO = "0"
+ONE = "1"
+DOT = "."
+STAR = "*"
+HASH = "#"
+DASH = "-"
+EQUAL = "="
+FULL = 80
+HALF = 40
+QUARTER = 20
+TINY = 10
+
+def b(text):
+    return f"\033[94m{text}\033[0m"
+
+def g(text):
+    return f"\033[92m{text}\033[0m"
+
+def r(text):
+    return f"\033[91m{text}\033[0m"
+
+def lead_zero(num, num_digits = 2):
+    zeroes = num_digits - len(str(num))
+    return f"{'0' * zeroes}{num}" if zeroes >= 0 else f"{num}"
+
+def rpad(text, length = 5):
+    text = str(text)
+    spaces = length - len(text)
+    return f"{text}{' ' * spaces if spaces > 0 else ''}"
+
+def lpad(text, length = 5):
+    text = str(text)
+    spaces = length - len(text)
+    return f"{' ' * spaces if spaces > 0 else ''}{text}"
+
+def id(num, length = 12):
+    return b(rpad(f"{num}:", length))
+
+def print_divider(divider=EQUAL, length=FULL):
+    print(divider*length)
+
+def print_grid(grid, coords=(None, None), neighbors=set(), missing_vals=[]):
+    top = " " * 5
+    middle = " " * 5
+    bottom = " " * 5
+    for i in range(len(grid[0])):
+        if i < 10:
+            top += " "
+            middle += " "
+            bottom += f"{i}"
+            continue
+        elif i < 100:
+            i = str(i)
+            top += f" "
+            middle += f"{i[0]}"
+            bottom += f"{i[1]}"
+        else:
+            i = str(i)
+            top += f"{i[0]}"
+            middle += f"{i[1]}"
+            bottom += f"{i[2]}"
+    print(top)
+    print(middle)
+    print(bottom)
+    for row_index, row in enumerate(grid):
+        output = ""
+        for col_index, col in enumerate(row):
+            if (row_index, col_index) == coords:
+                output += f"{g(col)}"
+            elif (row_index, col_index) in neighbors:
+                output += b("0")
+            elif col == WALL:
+                output += "\033[37;2m#\033[0m"
+            elif (row_index, col_index) in missing_vals:
+                output += f"\033[91;1;4m{col}\033[0m"
+            else:
+                output += f"\033[32;1m{col}\033[0m"
+        print(f"{lpad(row_index, 3)}: {output}")
+
+def exit():
+    print("Exiting...")
+    sys.exit()
+
+if len(sys.argv) < 2:
+    print("Please specify input file.")
+    exit()
+
+if not sys.argv[1].startswith("input"):
+    print("Invalid input file provided.")
+    exit()
+
+if len(sys.argv) > 2:
+    print("Unrecognized arguments provided.")
+    exit()
+
+edges  = defaultdict(set)
+with open(sys.argv[1], "r") as f:
+    for row in f.readlines():
+        c1, c2 = row.strip().split("-")
+        edges[c1].add(c2)
+        edges[c2].add(c1)
+
+def explore_neighbors(node, edges, subgraph=set(), depth=0):
+    for n in edges[node]:
+        if n in subgraph: continue
+
+        if subgraph.issubset(edges[n]):
+            subgraph.add(n)
+            explore_neighbors(n, edges, subgraph.copy(), depth+1)
+
+    return subgraph
+
+subgraphs = set()
+for c1 in edges:
+    subgraphs.add(tuple(explore_neighbors(c1, edges, set([c1]))))
+
+for x in sorted([*subgraphs], key=len, reverse=True):
+    print(",".join(sorted([*x])))
+    break
